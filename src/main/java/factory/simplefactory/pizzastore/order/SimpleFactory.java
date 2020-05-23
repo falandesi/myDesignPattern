900eccf6f88f75eb57b3dbbc87ae61e1bfbd1d1e
falandesi <falandesi@gmail.com>
2020-05-23 20:13:23 +0800

    fix create pizza type error in simplefactory

--- src/main/java/factory/simplefactory/pizzastore/order/SimpleFactory.java
+++ src/main/java/factory/simplefactory/pizzastore/order/SimpleFactory.java
@@ -3,6 +3,7 @@ package factory.simplefactory.pizzastore.order;
 
 import factory.simplefactory.pizzastore.pizza.CheesePizza;
 import factory.simplefactory.pizzastore.pizza.GreekPizza;
+import factory.simplefactory.pizzastore.pizza.PepperPizza;
 import factory.simplefactory.pizzastore.pizza.Pizza;
 
 //采用简单工厂模式 管理披萨 order 只需要使用工厂即可
@@ -20,7 +21,7 @@ public class SimpleFactory {
             pizza = new CheesePizza();
             pizza.setName(" 奶酪披萨 ");
         } else if ("pepper".equals(orderType)){
-            pizza = new CheesePizza();
+            pizza = new PepperPizza();
             pizza.setName(" 胡椒披萨 ");
         }
 
